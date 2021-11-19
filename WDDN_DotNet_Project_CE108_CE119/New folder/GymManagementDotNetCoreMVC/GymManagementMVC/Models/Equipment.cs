246a1b0b4fdae41ba14729f1e8a30bfabb5cf976
Dhruv <190310patel@gmail.com>
2021-11-20 00:33:42 +0530

    final

---
+++ WDDN_DotNet_Project_CE108_CE119/New folder/GymManagementDotNetCoreMVC/GymManagementMVC/Models/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace GymManagementMVC.Models
+{
+    public class Equipment
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name can't be empty!")]
+        public string EquipmentName { get; set; }
+        [Required(ErrorMessage = "Price can't be empty!")]
+        public decimal Quantity { get; set; }
+        [Required(ErrorMessage = "Field can't be empty!")]
+        public string MuscleFocus { get; set; }
+        [Required(ErrorMessage = "Weight can't be empty!")]
+        public decimal Weight { get; set; }
+    }
+}
---
+++ WDDN_DotNet_Project_CE108_CE119/New folder/GymManagementDotNetCoreMVC/GymManagementMVC/Models/Trainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace GymManagementMVC.Models
+{
+    public class Trainer
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name can't be empty")]
+        public string TrainerName { get; set; }
+
+        [Required(ErrorMessage = "Gender can't be empty")]
+        public string Gender { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Phone Number Required!")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Entered phone format is not valid.")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Age can't be empty")]
+        public decimal Age { get; set; }
+        [Required(ErrorMessage = "Field can't be empty")]
+        public decimal Experience { get; set; }
+
+        [Required(ErrorMessage = "Field can't be empty")]
+        public string Expertise { get; set; }
+    }
+}
+
